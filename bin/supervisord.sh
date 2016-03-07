1b0bb789d8344311cd199f77ecb147003de5cd7c
178inaba <178inaba@users.noreply.github.com>
2016-03-07 22:40:43 +0900

    remove supervisord.ini

--- bin/supervisord.sh
+++ bin/supervisord.sh
@@ -3,4 +3,4 @@
 usermod -u ${NGINX_UID:-$(id -u nginx)} -o nginx
 groupmod -g ${NGINX_GID:-$(id -g nginx)} -o nginx
 
-exec supervisord -c /etc/supervisord.conf
+exec supervisord -n -c /etc/supervisord.conf
